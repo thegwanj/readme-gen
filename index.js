47dcd5895a4b1dfa0a9b40b6bcefc3561ab70de5
unknown <gwanjuchung@yahoo.com>
2022-08-05 23:26:14 -0700

    added in some comments to help guide the direction of the rest of the code

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // TODO: Include packages needed for this application
+// We need: inquirerer, fs, and generateMarkdown
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
+// We can use generateMarkdown for this
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
+// Function that will be called to put everything above together
 function init() {}
 
 // Function call to initialize app
