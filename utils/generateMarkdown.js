43a4a66444ee4bcc6f5448a2f442be6186e63eab
unknown <gwanjuchung@yahoo.com>
2022-08-06 22:45:58 -0700

    created functions and added them to the generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,20 +8,65 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license.show == false){
+    return '';
+  } else{
+    renderLicenseBadge(license);
+    renderLicenseLink(license);
+  }
+}
 
 function renderTitle(title) {
   return `# ${title}`;
 }
 
-// data.title
-// data.description
-// data.authorName
+function renderDescription(description) {
+  return `${description}`;
+}
+
+function renderAuthorName(name) {
+  return `${name}`;
+}
+
+function renderHowTo(howTo){
+  return `${howTo}`;
+}
+
+function renderInstall(install) {
+  return `${install}`;
+}
+
+function renderReport(report) {
+  return `${report}`;
+}
+
+function renderContribute(contribute) {
+  return `${contribute}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `${renderTitle(data.title)}
 
+Created by: ${renderAuthorName(data.authorName)}
+## Description
+${renderDescription(data.description)}
+
+## Using the App
+${renderHowTo(data.howTo)}
+
+## Installation
+${renderInstall(data.install)}
+
+## Report Issues
+${renderReport(data.report)}
+
+## Contribute
+${renderContribute(data.contribute)}
+
+## Licenses
+${renderLicenseSection(data.license)}
 `;
 }
 
